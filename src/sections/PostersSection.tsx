7b36aaf5e61ee942a3ad99f6971be069d3b3ee4c
Sabat <sabat@hacknitr.com>
2026-03-11 15:04:20 +0530

    feat: Add `PostersSection` component with GSAP scroll animations for displaying show posters.

--- src/sections/PostersSection.tsx
+++ src/sections/PostersSection.tsx
@@ -20,11 +20,11 @@ const PostersSection = () => {
 
   return (
     <section className="posters-section bg-brand relative w-full h-[180dvh]">
-      <div className="flex items-center justify-center w-full ps-52 absolute 2xl:bottom-32 lg:bottom-0 bottom-[40vh]" style={{ transform: "translateY(-700px)" }}>
+      <div className="flex items-center justify-center w-full ps-52 absolute 2xl:bottom-32 lg:bottom-0 bottom-[40vh]" style={{ transform: "translateY(-500px)" }}>
         {showPosters.map((poster, index) => (
           <div
             key={index}
-            className={`posters-card ${poster.translation || ""} ${poster.rotation} lg:w-32 lg:-ms-20 2xl:w-96 2xl:-ms-44 w-80 -ms-44 flex-none md:rounded-[2vw] rounded-3xl overflow-hidden 2xl:relative absolute border-[.5vw] border-brand`}
+            className={`posters-card ${poster.translation || ""} ${poster.rotation} w-[18vw] -ms-[7.5vw] min-[2000px]:w-96 min-[2000px]:-ms-44 flex-none md:rounded-[2vw] rounded-3xl overflow-hidden relative min-[2000px]:relative border-[.5vw] border-brand`}
           >
             <img
               src={poster.src}
